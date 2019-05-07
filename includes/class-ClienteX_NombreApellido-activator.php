d05a900a04a2b3da71cd22d0936cdaae1872d7f5
Hember Colmenares <hemberfer@gmail.com>
2019-05-07 14:31:02 -0500

    fixes

--- includes/class-ClienteX_NombreApellido-activator.php
+++ includes/class-ClienteX_NombreApellido-activator.php
@@ -1,26 +1,42 @@
 <?php
-require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
-class ClienteX_NombreApellido_Activator {
+class ClienteX_NombreApellido_Activator
+{
 
-	public static function activate() {
-        self::db_plugin_sample();
-	}
+    public static function activate()
+    {
+        self::create_leads_table();
+        self::create_settings_table();
+    }
 
-   public static function db_plugin_sample() {
+    public static function create_leads_table()
+    {
 
         global $wpdb;
-        $nombreTabla = $wpdb->prefix . "demotabla";
+        $nombreTabla = $wpdb->prefix . "WPNombreAppellido_leads";
 
         $created = dbDelta(
             "CREATE TABLE $nombreTabla (
-      ID bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-      nombre varchar(60) NOT NULL DEFAULT '',
-      email varchar(64) NOT NULL DEFAULT '',
-      url varchar(100) NOT NULL DEFAULT '',
-      PRIMARY KEY (ID),
-      KEY email (email)
-    ) CHARACTER SET utf8 COLLATE utf8_general_ci;"
+            ID bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+            name varchar(60) NOT NULL DEFAULT '',
+            email varchar(64) NOT NULL DEFAULT '',
+            gender char(1) NOT NULL DEFAULT '',
+            PRIMARY KEY (ID)
+          ) CHARACTER SET utf8 COLLATE utf8_general_ci;"
+        );
+    }
+    public static function create_settings_table()
+    {
+
+        global $wpdb;
+        $nombreTabla = $wpdb->prefix . "WPNombreAppellido_settings";
+
+        $created = dbDelta(
+            "CREATE TABLE $nombreTabla (
+            config varchar(60) NOT NULL DEFAULT '',
+            value varchar(255) NOT NULL DEFAULT ''
+          ) CHARACTER SET utf8 COLLATE utf8_general_ci;"
         );
     }
 
--- includes/class-ClienteX_NombreApellido-deactivator.php
+++ includes/class-ClienteX_NombreApellido-deactivator.php
@@ -1,9 +1,25 @@
 <?php
+require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
 class ClienteX_NombreApellido_Deactivator {
 
-	public static function deactivate() {
+    public static function deactivate()
+    {
+        self::drop_leads_table();
+        self::drop_settings_table();
+    }
 
-	}
+    public static function drop_leads_table()
+    {
+        global $wpdb;
+        $nombreTabla = $wpdb->prefix . "WPNombreAppellido_leads";
+        $wpdb->query( 'DROP TABLE IF EXISTS ' . $nombreTabla );
+    }
+    public static function drop_settings_table()
+    {
+        global $wpdb;
+        $nombreTabla = $wpdb->prefix . "WPNombreAppellido_settings";
+        $wpdb->query( 'DROP TABLE IF EXISTS ' . $nombreTabla );
+    }
 
 }
