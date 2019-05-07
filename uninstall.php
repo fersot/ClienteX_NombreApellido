14254e8f61b681d497233b958dfbbb9e9361b567
Hember Colmenares <hemberfer@gmail.com>
2019-05-07 14:32:38 -0500

    fixes

--- uninstall.php
+++ uninstall.php
@@ -3,9 +3,3 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
-
-global $wpdb;
-$nombreTabla = $wpdb->prefix . "WPNombreAppellido_leads";
-$wpdb->query( 'DROP TABLE IF EXISTS ' . $nombreTabla );
-$nombreTabla = $wpdb->prefix . "WPNombreAppellido_settings";
-$wpdb->query( 'DROP TABLE IF EXISTS ' . $nombreTabla );
